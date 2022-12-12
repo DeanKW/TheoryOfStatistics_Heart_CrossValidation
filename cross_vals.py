fd9d971f2a2198e0f0473248d4fc17f202512b0f
Dean Weiss <weiss.dean@gmail.com>
2022-12-12 03:41:34 +0000

    Calculate F1 scores

--- cross_vals.py
+++ cross_vals.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import KFold 
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, jaccard_score
+from sklearn.metrics import accuracy_score, jaccard_score, f1_score
 
 #Implementing cross validation
 def perform_cross_validation(num_folds, df):
@@ -9,6 +9,7 @@ def perform_cross_validation(num_folds, df):
 
     acc_score = []
     jacc_score = []
+    f1_scores = []
 
     for train_index , test_index in kf.split(X):
         X_train, X_test = X.iloc[train_index,:],X.iloc[test_index,:]
@@ -18,18 +19,23 @@ def perform_cross_validation(num_folds, df):
         pred_values = model.predict(X_test)
         acc = accuracy_score(y_test, pred_values)
         jacc = jaccard_score(y_test, pred_values)
+        f1_sc = f1_score(y_test, pred_values)
         acc_score.append(acc)
         jacc_score.append(jacc)
+        f1_scores.append(f1_sc)
 
     avg_acc_score = sum(acc_score)/num_folds
     avg_jacc_score = sum(jacc_score)/num_folds
+    avg_f1_score = sum(f1_scores)/num_folds
 
     print('accuracy of each fold - {}'.format(acc_score))
     print('Avg accuracy : {}'.format(avg_acc_score))
     print()
     print('Jaccard Score of each fold - {}'.format(jacc_score))
     print('Avg Jaccard : {}'.format(avg_jacc_score))
-
+    print()
+    print('F1 Score of each fold - {}'.format(f1_scores))
+    print('Avg F1 Score : {}'.format(avg_f1_score))
 
 
 #Implementing Multiple Prediction Cross Validation
@@ -39,6 +45,7 @@ def perform_MPCV(num_folds, df):
 
     acc_score = []
     jacc_score = []
+    f1_scores = []
 
     for test_index, train_index in kf.split(X):
         X_train, X_test = X.iloc[train_index,:],X.iloc[test_index,:]
@@ -48,14 +55,21 @@ def perform_MPCV(num_folds, df):
         pred_values = model.predict(X_test)
         acc = accuracy_score(y_test, pred_values)
         jacc = jaccard_score(y_test, pred_values)
+        f1_sc = f1_score(y_test, pred_values)
+
         acc_score.append(acc)
         jacc_score.append(jacc)
+        f1_scores.append(f1_sc)
 
     avg_acc_score = sum(acc_score)/num_folds
     avg_jacc_score = sum(jacc_score)/num_folds
+    avg_f1_score = sum(f1_scores)/num_folds
 
     print('accuracy of each fold - {}'.format(acc_score))
     print('Avg accuracy : {}'.format(avg_acc_score))
     print()
     print('Jaccard Score of each fold - {}'.format(jacc_score))
     print('Avg Jaccard : {}'.format(avg_jacc_score))
+    print()
+    print('F1 Score of each fold - {}'.format(f1_scores))
+    print('Avg F1 Score : {}'.format(avg_f1_score))
